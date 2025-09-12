afb68bd3453d4e6c0dd337d2ed0f8d68521886cd
Paolo Román <133106829+paoloroman@users.noreply.github.com>
2025-09-12 14:36:37 +0200

    Hemos realizado avances significativos creando el frontend que comunica con el backend

---
+++ User-Frontend/src/app/models/user.ts
@@ -0,0 +1,11 @@
+
+export class User {
+
+    id! : number;
+    nombre!: string;
+    apellidos!: string;
+    email!: string;
+    usuario!: string;
+    pwd!: string;
+    
+}
---
+++ User-Frontend/src/app/services/user.service.ts
@@ -0,0 +1,39 @@
+//Paolo Román
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private users: User[] = [];
+
+  private urlBackend: string = 'http://localhost:8080/api/users';
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<User[]> { // tenemos que devolver un observable con el metodo get de http lo que hacemos es cpnseguir los datos del backend 
+    return this.http.get<User[]>(this.urlBackend);
+  }
+
+  findById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.urlBackend}/${id}`); // `` usamos Backticks en vez de concatrenar con + +
+  }
+
+  create(user: User): Observable<User> {
+    return this.http.post<User>(this.urlBackend, user);
+  }
+
+  update(user: User): Observable<User> {
+    return this.http.put<User>(`${this.urlBackend}/${user.id}`, user);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.urlBackend}/${id}`)
+  }
+}
+
+
